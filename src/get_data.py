395f695da4ceffa62bfe69edb7f029a6b4a26c21
Dandi Yu <dandi0220@gmail.com>
2023-02-09 16:45:11 +0000

    stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -20,6 +20,8 @@ def get_data(config_path):
     #print(df.head())
     return df
 
+#extra comment
+
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default= "params.yaml")

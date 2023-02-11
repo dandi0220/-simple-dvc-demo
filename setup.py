5ec682241a3bea38faab4ebf024e224ea7f19195
Dandi Yu <dandi0220@gmail.com>
2023-02-11 12:16:06 +0000

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='its a wine Q package',
+    author='dandi0220',
+    license='MIT',
+)

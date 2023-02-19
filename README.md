c0f8af61c8b1d485354dbeb7de9ab69ca84ad477
Dandi Yu <dandi0220@gmail.com>
2023-02-19 15:07:19 +0000

    mlflow begins

--- README.md
+++ README.md
@@ -67,5 +67,18 @@ build your own package command
 ```
 python setup.py sdist bdist_wheel
 ```
+---
+create an artifacts folder
 
+mlflow server command -
+```
+mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./artifacts --host 0.0.0.0 -p 1234
+mlflow server \
+    --backend-store-uri sqlite:///mlflow.db \
+    --default-artifact-root ./artifacts 
+    --host 0.0.0.0 -p 1234
+
+
+mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./artifacts --host 127.0.0.1 -p 5000
+```
 
--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -13,6 +13,8 @@ from get_data import read_params
 import argparse
 import joblib
 import json
+import mlflow
+from urllib.parse import urlparse
 
 
 def eval_metrics(actual, pred):
@@ -42,48 +44,77 @@ def train_and_evaluate(config_path):
     train_x = train.drop(target, axis=1)
     test_x = test.drop(target, axis=1)
 
-    lr = ElasticNet(
-        alpha=alpha, 
-        l1_ratio=l1_ratio, 
-        random_state=random_state)
-    lr.fit(train_x, train_y)
-
-    predicted_qualities = lr.predict(test_x)
+    ##################changes for mlflow##################
+    mlflow_config = config["mlflow_config"]
+    remote_server_uri = mlflow_config["remote_server_uri"]
     
-    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    mlflow.set_tracking_uri(remote_server_uri)
 
-    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
-    print("  RMSE: %s" % rmse)
-    print("  MAE: %s" % mae)
-    print("  R2: %s" % r2)
+    mlflow.set_experiment(mlflow_config["experiment_name"])
 
-#####################################################
-    scores_file = config["reports"]["scores"]
-    params_file = config["reports"]["params"]
+    with mlflow.start_run(run_name=mlflow_config["run_name"]) as mlops_run:
 
-    with open(scores_file, "w") as f:
-        scores = {
-            "rmse": rmse,
-            "mae": mae,
-            "r2": r2
-        }
-        json.dump(scores, f, indent=4)
+    ###################changes for mlflow##################
 
-    with open(params_file, "w") as f:
-        params = {
-            "alpha": alpha,
-            "l1_ratio": l1_ratio,
-        }
-        json.dump(params, f, indent=4)
-#####################################################
+        lr = ElasticNet(
+            alpha=alpha, 
+            l1_ratio=l1_ratio, 
+            random_state=random_state)
+        lr.fit(train_x, train_y)
+
+        predicted_qualities = lr.predict(test_x)
+
+        (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+        #mlflow
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2", r2)
+
+        tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(
+                lr,
+                "model",
+                registered_model_name = mlflow_config["registered_model_name"]
+            )
+        else:
+            mlflow.sklearn.load_model(lr, "model")
 
 
-    os.makedirs(model_dir, exist_ok=True)
-    model_path = os.path.join(model_dir, "model.joblib")
 
-    joblib.dump(lr, model_path)
+        #print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        #print("  RMSE: %s" % rmse)
+        #print("  MAE: %s" % mae)
+        #print("  R2: %s" % r2)
 
+    ########################################################
+        #scores_file = config["reports"]["scores"]
+        #params_file = config["reports"]["params"]
+#
+        #with open(scores_file, "w") as f:
+        #    scores = {
+        #        "rmse": rmse,
+        #        "mae": mae,
+        #        "r2": r2
+        #    }
+        #    json.dump(scores, f, indent=4)
+#
+        #with open(params_file, "w") as f:
+        #    params = {
+        #        "alpha": alpha,
+        #        "l1_ratio": l1_ratio,
+        #    }
+        #    json.dump(params, f, indent=4)
+   #################################################
 
+        #os.makedirs(model_dir, exist_ok=True)
+        #model_path = os.path.join(model_dir, "model.joblib")
+#
+        #joblib.dump(lr, model_path)
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
